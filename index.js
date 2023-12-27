93d60e2eccc3101e04d2060b90a8d35c50db1fcc
Kylie Clark <kyliedarlene@icloud.com>
2023-12-27 13:10:16 -0700

    added random background generation; default HTML is commented out temporarily

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-// click event: change background picture
+// select default section
+// let defaultPage = document.querySelector(default);
+// console.log("default");
 
-// addEventListener("click", (changeBackground) => {});
+// set random background image on page load
+const backgroundImages = ["woodsy.jpeg", "party.jpeg", "nautiloid.jpeg", "battle.jpeg", "space.jpeg"];
+let currentBackground;
+setBackground();
 
+// function: set random background image
+
+function setBackground() {
+    const newBackground = backgroundImages[Math.floor(Math.random()*backgroundImages.length)]; // select random image from images array 
+    if (newBackground===currentBackground) { 
+        setBackground(); // prevent the same image from being loaded twice
+    } else {
+        document.querySelector('body').style.backgroundImage = `url('images/${newBackground}`; // set new background image in CSS
+        currentBackground = newBackground; // store name of current background under global variable currentBackground
+    }
+}
+
+// event listener: change background picture on button click
+
+const btn = document.querySelector('button');
+
+btn.addEventListener("click", () => {
+    // set new background
+    setBackground();
+
+
+});
