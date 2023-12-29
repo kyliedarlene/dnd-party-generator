12b79471eceb192c7548741c8c2ac857744214a2
Kylie Clark <kyliedarlene@icloud.com>
2023-12-28 21:52:00 -0700

    pseudocode for generating party object using .fetch()

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setBackground() {
         setBackground(); // prevent the same image from being loaded twice
     } else {
         document.querySelector('body').style.backgroundImage = `url('images/${newBackground}`; // set new background image in CSS
-        currentBackground = newBackground; // store name of current background under global variable currentBackground
+        currentBackground = newBackground;  // store name of current background under global variable currentBackground
     }
 }
 
@@ -23,6 +23,60 @@ const btn = document.querySelector('button');
 btn.addEventListener("click", () => {
     // set new background
     setBackground();
-
-
 });
+
+
+
+
+
+/******
+ * 
+ * PSEUDOCODE
+ * 
+ * When the button is clicked:
+ * 
+ *** setBackground()
+ * 
+ *** displayGeneratedParty()
+ * 
+ * 
+ * const party = generateParty()
+ * forEach element of party {
+ *      displayCard();                         /////
+ * }
+ * 
+ * generateParty() create Party Object + fill with Character Objects
+ *      declare party
+ *      x 6: addCharacter()
+ *      return party
+ * 
+ * addCharacter() add Character Object to Party Object          // maybe could be condensed into generateParty()?
+ *      add makeCharacter()
+ *      Name: Character1            // how to make it automatically character #?
+ * 
+ * makeCharacter() make and return Character object
+ *      Race: fetchStat()
+ *      Class: fetchStat()
+ *      Alignment: fetchStat()
+ */
+
+
+
+ /*
+ * 
+ * fetchAttribute()
+ *      
+ * 
+ * 
+ * 
+ */
+
+// const url = "https://www.dnd5eapi.co/docs/#get-/api/";
+
+// function fetchAttribute(attribute) {
+//     fetch(`url/${attribute}/elf`)
+//         .then((response) => response.json())
+//         .then(() => console.log(response))
+//  }
+
+//  fetchAttribute("race");
