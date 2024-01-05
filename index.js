3ab3a4dff3ff1801daabbddb6c205ddc93493720
Kylie Clark <kyliedarlene@icloud.com>
2024-01-04 21:15:32 -0700

    added Promise.all to generateCharacter so that returned object has accessible elements

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ const btn = document.querySelector('button');
 let currentBackground;
 const backgroundImages = ["woodsy.jpeg", "party.jpeg", "nautiloid.jpeg", "battle.jpeg", "space.jpeg"];
 
+const generatedCharacters = [];
 
-/** FUNCTIONS **/
+//** EVENT LISTENERS **/
+
+/* page load: set random background */
+window.addEventListener("load", setBackground);
+
+/* button click: reset background and load party (this will later be changed so that "click" calls the setParty function, and background will change after data fetches) */
+btn.addEventListener("click", async() => {
+    setBackground();
+    createCard(await generateCharacter());
+});
+
+
+
+//** FUNCTIONS **//
 
 /* set random background image */
 
 function setBackground() {
-    const newBackground = backgroundImages[Math.floor(Math.random()*backgroundImages.length)]; // select random image name from images array 
+    const x = randomElement(backgroundImages);
+    const newBackground = backgroundImages[x]; // select random image name from images array 
     if (newBackground===currentBackground) { 
         setBackground(); // prevent the same image from being loaded twice
     } else {
@@ -21,21 +36,72 @@ function setBackground() {
         currentBackground = newBackground; // store name of current background under global variable currentBackground
     }
 }
-// TESTING: CODE REFACTOR
 
-//createCard() adds card to DOM
-const cardContainer = document.getElementById('card-container');
+
+// uses fetch to retrieve random character data and return character object
+
+async function generateCharacter() {
+    const randomCharacter = {
+        race: {},
+        class: {
+            archetype: {}, 
+        },
+        background: {},
+    };
+
+    await Promise.all([     // fetch data and add 
+
+    // race
+        fetch(`https://api.open5e.com/races`)
+            .then((response) => response.json())
+            .then((data) => {
+            const x = randomElement(data.results);
+            randomCharacter.race.name = data.results[x].name;
+        }),
+        // class
+        fetch(`https://api.open5e.com/classes`)
+            .then((response) => response.json())
+            .then((data) => {
+            const x = randomElement(data.results);
+            randomCharacter.class.name = data.results[x].name;
+
+            // archetype
+            const y = randomElement(data.results[x].archetypes);
+            randomCharacter.class.archetype.name = data.results[x].archetypes[y].name;
+            randomCharacter.class.archetype.description = data.results[x].archetypes[y].desc;
+        }),
+
+        // background
+        fetch(`https://api.open5e.com/backgrounds`)
+            .then((response) => response.json())
+            .then((data) => {
+            const x = randomElement(data.results);
+            randomCharacter.background.name = data.results[x].name;
+            randomCharacter.background.description = data.results[x].desc;
+            randomCharacter.background.proficiencies = data.results[x].skill_proficiencies;
+        })
+    ])
+    // console.log("generateCharacter start")
+    // console.log(randomCharacter.race.name)
+    // console.log("generateCard end")
+    // generatedCharacters.push(randomCharacter);
+    
+    //console.log(randomCharacter)
+
+    return randomCharacter;
+}
+
+
+//createCard() creates card and appends it to carousel
 
 function createCard(character) {
-    const card = document.createElement('div');
-    card.id = 'card';
-    card.class = 'card';
-    cardContainer.append(card);
+    const card = document.createElement('li');
+    card.className = 'card';
 
     const characterRace = document.createElement('h3');
     const characterClass = document.createElement('h2');
     const classArchetype = document.createElement('h3');
-    //const archetypeDescription = document.createAttribute('p');
+    const archetypeDescription = document.createElement('p');
 
     characterRace.id = 'race';
     characterClass.id = 'class';
@@ -44,84 +110,45 @@ function createCard(character) {
 
     characterRace.innerText = character.race.name;
     characterClass.innerText = character.class.name;
-    classArchetype.innerText = character.class.archetype;
-    //archetypeDescription.innerText = "The Circle of the Land is made up of mystics and sages who safeguard ancient knowledge and rites through a vast oral tradition. These druids meet within sacred circles of trees or standing stones to whisper primal secrets in Druidic. The circle's wisest members preside as the chief priests of communities that hold to the Old Faith and serve as advisors to the rulers of those folk. As a member of this circle, your magic is influenced by the land where you were initiated into the circle's mysterious rites."
+    classArchetype.innerText = character.class.archetype.name;
+    //archetypeDescription.innerText = character.class.archetype.description;
 
     card.append(characterRace);
     card.append(characterClass);
     card.append(classArchetype);
-    //card.appendChild(archetypeDescription);
+    //card.append(archetypeDescription);
+
+    // console.log("createCard start");
+    // console.log(character);
+    // console.log("createCard end")
+
+    carousel.append(card);
 }
-
-//createCard();
-
-// uses fetch to retrieve random character data and then save in object; this will be passed to the create card function
-// returns character object
-
-function generateCharacter() {
-    const randomCharacter = {
-        race: {},
-        class: {
-            archetype: {}, 
-        },
-        background: {},
-    };
-    
-    // race
-    fetch(`https://api.open5e.com/races`)
-        .then((response) => response.json())
-        .then((data) => assignAttributes(data, randomCharacter, "race"))
-    //     .then((data) => {
-    //     const x = randomElement(data.results);
-    //     randomCharacter.race.name = data.results[x].name;
-    //     randomCharacter.race.description = data.results[x].desc;
-    //     //console.log(randomCharacter);
-    // })
-
-    // class
-    fetch(`https://api.open5e.com/classes`)
-        .then((response) => response.json())
-        .then((data) => {
-        const x = randomElement(data.results);
-        randomCharacter.class.name = data.results[x].name;
-        randomCharacter.class.description = data.results[x].desc;
-        //console.log(randomCharacter);
-
-        // archetype
-        const y = randomElement(data.results[x].archetypes);
-        //console.log(data.results[x].archetypes[y].name)
-        randomCharacter.class.archetype.name = data.results[x].archetypes[y].name;
-        randomCharacter.class.archetype.description = data.results[x].archetypes[y].desc;
-        //console.log(randomCharacter)
-
-    })
-
-    // background
-    fetch(`https://api.open5e.com/backgrounds`)
-        .then((response) => response.json())
-        .then((data) => {
-        const x = randomElement(data.results);
-        randomCharacter.background.name = data.results[x].name;
-        randomCharacter.background.description = data.results[x].desc;
-        randomCharacter.background.proficiencies = data.results[x].skill_proficiencies;
-    })
-
-    return randomCharacter;
-}
-
-let generatedCharacter = generateCharacter();
-console.log(generatedCharacter);
 
 
 function randomElement(array) {
     return Math.floor(Math.random() * array.length);
 }
 
-function assignAttributes(data, character, attribute) {
-    const x = randomElement(data.results);
-    character[attribute].name = data.results[x].name;
-    character[attribute].desc = data.results[x].desc;
-}
+/* invoke functions */
+
+
+// console.log(generatedCharacter.race)
+
+
+//createCard(generatedCharacter);
+
+
+// console.log(generatedCharacter);
+// //console.log(generateCharacter.race['name'])
+// console.log("debugging end")
+
+// console.log(generatedCharacters);
+
+
+
+
+
 
 
 // OLD CODE: testing refactored code
@@ -160,28 +187,3 @@ function assignAttributes(data, character, attribute) {
 //         carousel.append(card);
 //     }
 // }
-
-// /* Fetch attribute and append to card */
-
-// function setAttribute(card, cardItem, attribute) {
-//     fetch(`https://api.open5e.com/${attribute}`)
-//         .then((response) => response.json())
-//         .then((data) => {
-//             const x = Math.floor(Math.random() * data.count);
-//             //console.log(data.results[x])
-//             cardItem.innerText = data.results[x].name;
-//             cardItem.class = attribute;
-//             card.append(cardItem);
-//     })
-// }
-
-//** EVENT LISTENERS **/
-
-/* page load: set random background */
-window.addEventListener("load", setBackground);
-
-/* button click: reset background and load party (this will later be changed so that "click" calls the setParty function, and background will change after data fetches) */
-btn.addEventListener("click", () => {
-    setBackground();
-    //generateParty();
-});
